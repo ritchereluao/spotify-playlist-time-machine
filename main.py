d90d3c1e14b34201cd57daef253a8914faa0ee2c
Ritche Reluao <34097542+ritchereluao@users.noreply.github.com>
2021-11-24 15:56:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ soup = BeautifulSoup(response.text, "html.parser")
 song_titles = soup.select("li ul h3")
 song_names = [each_song.getText().strip() for each_song in song_titles]
 
-
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id="YOUR CLIENT ID",
     client_secret="YOUR CLIENT SECRET",
@@ -23,7 +22,6 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
 ))
 
 user_id = sp.current_user()["id"]
-
 song_uris = []
 year = date.split("-")[0]
 for song in song_names:
@@ -33,7 +31,6 @@ for song in song_names:
         song_uris.append(uri)
     except IndexError:
         print(f"{song} -- skipped.")
-
 
 playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
 sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
