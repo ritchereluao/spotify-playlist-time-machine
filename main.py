356472bfb9bffebb9982618c9069824670783174
Ritche Reluao <34097542+ritchereluao@users.noreply.github.com>
2021-11-24 15:24:39 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import pprint
+
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+URL = f"https://www.billboard.com/charts/hot-100/{date}/"
+
+response = requests.get(url=URL)
+soup = BeautifulSoup(response.text, "html.parser")
+
+song_titles = soup.select("li ul h3")
+song_names = [each_song.getText().strip() for each_song in song_titles]
+
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id="YOUR CLIENT ID",
+    client_secret="YOUR CLIENT SECRET",
+    redirect_uri="http://example.com",
+    scope="playlist-modify-private",
+    cache_path="token.txt"
+))
+
+user_id = sp.current_user()["id"]
+
+song_uris = []
+year = date.split("-")[0]
+result = sp.search(q=f"track:Incomplete year:{year}", type="track")
+# pprint.pprint(result)
+# song_uri = result["tracks"]["items"][0]["uri"]
+# print(song_uri)
+
+
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    # print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} -- skipped.")
+
+
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+# print(playlist)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
